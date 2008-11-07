552ff7454c885da808641e050715739565e11659
Greg Sabino Mullane <greg@endpoint.com>
2008-11-07 09:04:16 -0500

    Bump version to 2.4.3

--- check_postgres.pl
+++ check_postgres.pl
@@ -28,7 +28,7 @@ $Data::Dumper::Varname = 'POSTGRES';
 $Data::Dumper::Indent = 2;
 $Data::Dumper::Useqq = 1;
 
-our $VERSION = '2.4.2';
+our $VERSION = '2.4.3';
 
 use vars qw/ %opt $PSQL $res $COM $SQL $db /;
 
@@ -3725,7 +3725,7 @@ sub show_dbstats {
 =head1 NAME
 
 B<check_postgres.pl> - a Postgres monitoring script for Nagios, MRTG, Cacti, and others
-This documents describes check_postgres.pl version 2.4.2
+This documents describes check_postgres.pl version 2.4.3
 
 =head1 SYNOPSIS
 
@@ -4941,6 +4941,10 @@ https://mail.endcrypt.com/mailman/listinfo/check_postgres-announce
 Items not specifically attributed are by Greg Sabino Mullane.
 
 =over 4
+
+=item B<Version 2.4.3>
+
+  Fix options for replicate_row action, per report from Jason Gordon.
 
 =item B<Version 2.4.2>
 
