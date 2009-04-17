f23f54470b126b9e167f916c40ce6a731f7bc781
Greg Sabino Mullane <greg@endpoint.com>
2009-04-17 10:23:13 -0400

    Add bad_fake_version() func back in.

--- t/CP_Testing.pm
+++ t/CP_Testing.pm
@@ -387,6 +387,28 @@ SELECT 'PostgreSQL $version on fakefunction for check_postgres.pl testing'::text
 
 } ## end of fake version
 
+
+sub bad_fake_version {
+
+	my $self = shift;
+	my $version = shift || '9.9';
+	my $dbh = $self->{dbh} || die;
+	my $dbuser = $self->{testuser} || die;
+
+	$dbh->do(qq{
+CREATE OR REPLACE FUNCTION public.version()
+RETURNS TEXT
+LANGUAGE SQL
+AS \$\$
+SELECT 'Postgres $version on fakefunction for check_postgres.pl testing'::text;
+\$\$
+});
+	$dbh->do("ALTER USER $dbuser SET search_path = public, pg_catalog");
+	$dbh->commit();
+
+} ## end of bad fake version
+
+
 sub reset_path {
 
 	my $self = shift;
