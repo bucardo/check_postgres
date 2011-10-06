20c2c42eff6f3c500a77ecce68d4708fde74c947
Mike Blackwell <mike.blackwell@rrd.com>
2011-10-06 16:15:40 -0400

    Show dbservice if used at start of same_schema header.

--- check_postgres.pl
+++ check_postgres.pl
@@ -1372,8 +1372,9 @@ sub add_response {
                     'CP version',
                     $row->{cversion};
             }
-            $same_schema_header .= sprintf "\nDB %s: %s%s%s%s",
+            $same_schema_header .= sprintf "\nDB %s: %s%s%s%s%s",
                 $number,
+                defined $row->{dbservice} ? qq{dbservice=$row->{dbservice} } : '',
                 defined $row->{port} ? qq{port=$row->{port} } : '',
                 defined $row->{host} ? qq{host=$row->{host} } : '',
                 defined $row->{dbname} ? qq{dbname=$row->{dbname} } : '',
