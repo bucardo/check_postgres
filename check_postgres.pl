d53d58e7ceae4e6e9d46e10a77dc2725b7388092
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2018-11-22 11:58:33 +0000

    Fixing duplicate perfdata if there are multiple rows
    We need to call `add_ok` once for each instance of `$db` otherwise perfdata doesn't work at all, more frequently and it is duplicated

--- check_postgres.pl
+++ check_postgres.pl
@@ -5527,8 +5527,8 @@ sub check_streaming_delta {
                     add_warning "WARNING for : $row->{'client_addr'} - $row->{'application_name'} - $wal_type";
                 }
             }
-            add_ok "OK for : $row->{'client_addr'} - $row->{'application_name'}";
         }
+        add_ok "OK for : $row->{'client_addr'} - $row->{'application_name'}";
     }
 
     return;
